37ff785c166e0581693c8d80340b716120dd2433
tung <tungng14@gmail.com>
2023-01-06 05:02:30 +0700

    upd

--- footerAndHeader/header.php
+++
@@ -1,7 +0,0 @@
-<header class="d-flex justify-content-center py-3">
-    <ul class="nav nav-pills">
-        <li class="nav-item"><a href="" class="nav-link active" aria-current="page" onClick="refresh(this)">Trang Chủ</a></li>
-        <li class="nav-item"><a href="https://www.github.com/TrHgTung/bai_xi_dzach" target="_blank" class="nav-link">Mã Nguồn</a></li>
-        <li class="nav-item"><a href="https://www.github.com/TrHgTung" target="_blank" class="nav-link">Tác Giả</a></li>
-    </ul>
-</header>
